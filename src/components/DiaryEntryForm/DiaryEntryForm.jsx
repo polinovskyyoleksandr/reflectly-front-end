694d0cd8821f0fb5a90ba0f621bb9a3e51dc4fb0
Frances Woodall <franceswoodall@hotmail.co.uk>
2026-02-10 11:53:22 +0000

    basic scaffolding for diaryEntryForm created

---
+++ src/components/DiaryEntryForm/DiaryEntryForm.jsx
@@ -0,0 +1,86 @@
+import { useState } from 'react'; 
+
+const DiaryEntryForm = (props) => {
+    const [formData, setFormData] = useState({
+        title: "", 
+        reflection: "", 
+        isEntryPublic: false, 
+        isEntryUsernamePublic: false, 
+        mood: "happy", 
+        moodLvl: 5
+    }); 
+
+const handleChange = (evt) => {
+    const { name, value, type, checked } = evt.target; 
+
+    setFormData({ ... formData, [name]: type === "checkbox" ? checked: value 
+    });  
+}; 
+
+const handleSubmit = (evt) => {
+    evt.preventDefault(); 
+    console.log('form data', formData)
+}; 
+
+ return (
+    <main>
+      <form onSubmit={handleSubmit}>
+        <h1> New Diary Entry </h1>
+
+        <label htmlFor="title-input">Title</label>
+        <input
+          type="text"
+          name="title"
+          id="title-input"
+          value={formData.title}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="reflection-input">Reflections</label> 
+        <input
+          type="text"
+          name="reflection"
+          id="reflection-input"
+          value={formData.reflection}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="mood">What is your current mood?</label>
+        <select
+          required
+          name="mood"
+          id="mood-input"
+          value={formData.mood}
+          onChange={handleChange}
+        >
+          <option value="Happy">Happy</option>
+          <option value="Sad">Sad</option>
+          <option value="Bored">Bored</option>
+        </select>
+
+        <label htmlFor="moodLvl">What is the intensity of your current mood?</label>
+        <input 
+            type="number"
+            name="moodLvl"
+            id="moodLvl"
+            min="1"
+            max="10"
+            value = {formData.moodLvl} 
+            onChange={handleChange} 
+        />
+        <div>
+            <label>
+                <input type="checkbox" 
+                name="isEntryUsernamePublic" 
+                checked={formData.isEntryUsernamePublic}
+                onChange={handleChange}
+                />
+            </label>  
+        </div>
+
+        <button type="submit">Create Entry</button>
+    
+      </form>
+    </main>
+  );
+};
